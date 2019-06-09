7025ea15ec2d53cad62057e9680c90bd510860a8
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2019-06-09 10:45:06 -0700

    warmup done

--- hw1/warmup.py
+++ hw1/warmup.py
@@ -1,7 +1,11 @@
 import numpy as np
 import tensorflow as tf
 import pdb
+import matplotlib.pyplot as plt
 
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 class WarmUp:
 
@@ -46,19 +50,67 @@ class WarmUp:
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
         self.update_op = self.optimizer.minimize(self.loss)
 
+        # sampling operation
+        dist = tf.distributions.Categorical(probs=self.probs)
+        self.get_sample_op = dist.sample()
 
-    def train(self, train_data, niter=1000, batch_size=64):
+    def train(self, sess, train_data, valid_data=None, niter=1000, batch_size=64):
 
-        with tf.Session() as sess:
-            init_op = tf.global_variables_initializer()
-            sess.run(init_op)
-            for itr in range(niter):
-                xdata = np.random.choice(train_data, batch_size, replace=False)
-                feed_dict = {self.data_ph: xdata}
-                loss_m = sess.run(self.loss, feed_dict)
-                sess.run(self.update_op, feed_dict)
-                loss_p = sess.run(self.loss, feed_dict)
-                print(f'[info] loss- = {loss_m}, loss+ = {loss_p}')
+        train_xvar, valid_xvar = [], []
+        train_yvar, valid_yvar = [], []
+
+        for itr in range(niter):
+            xdata = np.random.choice(train_data, batch_size, replace=False)
+            feed_dict = {self.data_ph: xdata}
+            _, train_loss = sess.run([self.update_op, self.loss], feed_dict)
+
+            train_xvar.append(itr)
+            train_yvar.append(train_loss)
+
+            if itr % (niter // 10) == 0:
+                print(f'[info] iter {itr}: train_loss={train_loss}')
+                if valid_data is not None:
+                    valid_loss = sess.run(self.loss, feed_dict={self.data_ph: valid_data})
+                    valid_xvar.append(itr)
+                    valid_yvar.append(valid_loss)
+
+        return dict(
+            train_x=train_xvar,
+            train_y=train_yvar,
+            valid_x=valid_xvar,
+            valid_y=valid_yvar
+        )
+
+
+    def get_pdist(self, sess):
+        x = np.arange(1, 101, 1)
+        p_dist = sess.run(self.probs, feed_dict={self.data_ph: x})
+
+        samples = []
+        for _ in range(1000):
+            samples.append(sess.run(self.get_sample_op))
+
+        return dict(
+            x=x,
+            pmodel=p_dist,
+            samples=samples
+        )
+
+    @staticmethod
+    def print_results(results):
+        # print log loss of both training and validation datasets
+        plt.figure(1)
+        plt.plot(results['train_x'], results['train_y'], color='r', label='train')
+        plt.plot(results['valid_x'], results['valid_y'], color='b', label='valid')
+        plt.title('negative log likelihood')
+        plt.legend()
+
+        plt.figure(2)
+        plt.bar(results['prob']['x'], results['prob']['pmodel'], color='r', label='model prob')
+        plt.hist(results['prob']['samples'], bins=results['prob']['x'], density=True,
+                 label='estimated prob', color='b', alpha=0.2)
+        plt.legend()
+        plt.show()
 
     def main(self):
         data = self.sample_data()
@@ -68,7 +120,15 @@ class WarmUp:
         valid_data = data[train_idx:]
 
         self.make_flow()
-        self.train(train_data, niter=10000, batch_size=512)
+        with tf.Session() as sess:
+            init_op = tf.global_variables_initializer()
+            sess.run(init_op)
+            # deliverables
+            results = self.train(sess, train_data, valid_data, niter=100, batch_size=2048)
+            prob_dist = self.get_pdist(sess)
+            results['prob'] = prob_dist
+
+        self.print_results(results)
 
 if __name__ == '__main__':
     agent = WarmUp()
