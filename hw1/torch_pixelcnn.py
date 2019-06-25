2a2e9402c43c2693ad62e17bd8b40e1af02a1fff
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2019-06-24 22:15:48 -0700

    cuda

--- hw1/torch_pixelcnn.py
+++ hw1/torch_pixelcnn.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     agent =  ARPixelCNN(file,
                         nepochs=50,
                         learning_rate=1e-3,
-                        batch_size=56,
+                        batch_size=60,
                         feature_size=128,
                         )
     agent.main()
